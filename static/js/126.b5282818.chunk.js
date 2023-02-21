5a2613d82e60f44fa1c97682a7210b648d162f00
Dmitriy-Tyukh <99756564+Dmitriy-Tyukh@users.noreply.github.com>
2023-02-21 13:17:37 +0000

    Deploying to gh-pages from @ Dmitriy-Tyukh/goit-react-hw-08-phonebook@7067fbe63d5413849433d652c3fc53fab7931236 🚀

---
+++ static/js/126.b5282818.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[126],{9126:function(t,o,e){e.d(o,{HHm:function(){return r}});var n=e(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"}}]})(t)}}}]);
+//# sourceMappingURL=126.b5282818.chunk.js.map
